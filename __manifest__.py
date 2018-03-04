e19222e84bbcc90111db542dd6698d34aa720484
Alfie Qashwa <alfieqashwa@gmail.com>
2018-03-04 12:37:06 +0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+{
+    'name': 'Cello Theme',
+    'description': 'Create theme for tutorial and will be developed further',
+    'version': '1.0',
+    'author': 'Alfie Qashwa',
+
+    'data': [
+    ],
+    'category': 'Theme/Creative',
+    'depends': ['website'],
+}
